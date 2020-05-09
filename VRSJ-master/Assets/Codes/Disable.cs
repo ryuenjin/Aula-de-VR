1f68a79b27f56a6a9f719f0fb7e3ca5f70794bba
ryuenjin <54440789+ryuenjin@users.noreply.github.com>
2020-05-08 21:51:07 -0300

    Criação de Puzzle

---
+++ VRSJ-master/Assets/Codes/Disable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+    public AudioSource locksound;
+    public GameObject toDisable;
+    public Rigidbody rdb;
+
+    public void ButtonAction()
+    {
+        locksound.Play();
+
+        if (toDisable)
+        {
+            toDisable.SetActive(false);
+        }
+
+        if (rdb)
+        {
+            rdb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (locksound)
+        {
+            locksound.Play();
+        }
+    }
+
+}
---
+++ VRSJ-master/Assets/Codes/Door.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Transform doorMesh;
+    bool closed=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(Abre());
+    }
+
+   
+    public IEnumerator Abre()
+    {
+        float ang=0;
+        while (ang>-148)
+        {
+            ang = Mathf.Lerp(ang, -150,Time.deltaTime);
+            doorMesh.transform.rotation = Quaternion.Euler(0, ang, 0);
+          
+            yield return new WaitForEndOfFrame();
+
+        }
+        closed = false;
+    }
+
+    public IEnumerator Fecha()
+    {
+        float ang = -150;
+        while (ang<0)
+        {
+            ang = Mathf.Lerp(ang, 1, Time.deltaTime);
+            doorMesh.transform.rotation = Quaternion.Euler(0, ang, 0);
+
+            yield return new WaitForEndOfFrame();
+
+        }
+
+        closed = true;
+
+    }
+
+    public void ButtonAction()
+    {
+        if (closed)
+        {
+            StartCoroutine(Abre());
+
+        }
+        else
+        {
+            StartCoroutine(Fecha());
+        }
+    }
+
+}
